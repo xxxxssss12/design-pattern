a6ea069d08d68244506ab389bf2f3f406bcef0e7
xs <xs6666@qq.com>
2017-02-19 15:28:23 +0800

    xx

---
+++ abstract-factory/src/main/java/xs/design/abstract_factory/FactoryProducer.java
@@ -0,0 +1,15 @@
+package xs.design.abstract_factory;
+
+/**
+ * Created by hasee on 2017/2/19.
+ */
+public class FactoryProducer {
+    public static AbstractFactory getFactory(String choice){
+        if(choice.equalsIgnoreCase("SHAPE")){
+            return new ShapeFactory();
+        } else if(choice.equalsIgnoreCase("COLOR")){
+            return new ColorFactory();
+        }
+        return null;
+    }
+}
